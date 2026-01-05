cb5a35b5a0a380bb8d2bcce914109dc19151d416
Yannik <115665549+yannikosterholzer@users.noreply.github.com>
2026-01-06 00:01:15 +0100

    Create bcu.sv (Branch Compare Unit)

---
+++ src/bcu.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module bcu(
+    input  logic [31:0] rs1_i,
+    input  logic [31:0] rs2_i,
+    input  logic [2:0]  bcu_op_i,
+    output logic        tk_brnch_o  // take Branch
+    );
+    
+    always_comb begin 
+        case(bcu_op_i)
+            3'b000:  tk_brnch_o = (rs1_i == rs2_i);
+            3'b001:  tk_brnch_o = (rs1_i != rs2_i);
+            3'b100:  tk_brnch_o = ($signed(rs1_i) <  $signed(rs2_i));
+            3'b101:  tk_brnch_o = ($signed(rs1_i) >= $signed(rs2_i));
+            3'b110:  tk_brnch_o = (rs1_i <  rs2_i);
+            3'b111:  tk_brnch_o = (rs1_i >= rs2_i);
+            default: tk_brnch_o = 1'b0;
+        endcase
+    end
+endmodule
