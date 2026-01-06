04d51cd1d23e2955593b6f29f91baa7471de0638
Yannik <115665549+yannikosterholzer@users.noreply.github.com>
2026-01-06 17:39:59 +0100

    fix: correct SRAI implementation to ensure proper sign extension

--- src/alu.sv
+++ src/alu.sv
@@ -11,15 +11,23 @@ module alu(
      );
     
     always_comb begin
+        alu_o = 32'b0;
         unique casez(alu_op_i)
-        3'b000 :	alu_o = (sbtr_i)? (op1_i - op2_i) : (op1_i + op2_i);
-        3'b001 :	alu_o = op1_i << shamt_i;
-        3'b010 :	alu_o = ($signed(op1_i) < $signed(op2_i));
-        3'b011 :	alu_o = ((op1_i) < (op2_i));
-        3'b100 :	alu_o = (op1_i ^ op2_i);
-        3'b101 :	alu_o = (shdir_i)?(op1_i >>> shamt_i) : (op1_i >> shamt_i);
-        3'b110 :	alu_o = ((op1_i) | (op2_i));
-        3'b111 :	alu_o = ((op1_i) & (op2_i));  
+            3'b000 : alu_o = (sbtr_i) ? (op1_i - op2_i) : (op1_i + op2_i);
+            3'b001 : alu_o = op1_i << shamt_i;
+            3'b010 : alu_o = {31'b0, ($signed(op1_i) < $signed(op2_i))};
+            3'b011 : alu_o = {31'b0, (op1_i < op2_i)};
+            3'b100 : alu_o = (op1_i ^ op2_i);   
+            3'b101 : begin
+                        if (shdir_i) 
+                            alu_o = $unsigned($signed(op1_i) >>> shamt_i); 
+                        else         
+                            alu_o = op1_i >> shamt_i;
+                     end     
+            3'b110 : alu_o = (op1_i | op2_i);
+            3'b111 : alu_o = (op1_i & op2_i);
+            default: alu_o = 32'b0;
         endcase
      end
+     
 endmodule
